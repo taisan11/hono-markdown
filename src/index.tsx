fabf44a35a3a828404ce8e49cc03f6ddefca3585
Kengo Watanabe <121782456+nabeken5@users.noreply.github.com>
2024-02-10 10:45:59 +0900

    feat: render markdown

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,28 @@
+import fs from 'node:fs'
 import { Hono } from 'hono'
+import MarkdownIt from 'markdown-it'
 import { renderer } from './renderer'
 
 const app = new Hono()
 
+const md = MarkdownIt({
+  html: true,
+  linkify: true
+})
+
+export const readFileSync = (path: string): string => {
+  const markdownStr = fs.readFileSync(path, { encoding: 'utf-8' })
+
+  return markdownStr
+}
+
 app.get('*', renderer)
 
 app.get('/', (c) => {
-  return c.render(<h1>Hello!</h1>)
+  const markdownStr = readFileSync('./docs/top.md')
+  const result = md.render(markdownStr);
+  // biome-ignore lint/security/noDangerouslySetInnerHtml: It's by markdown-it
+return  c.render(<div class="doc" dangerouslySetInnerHTML={{__html: result}} />)
 })
 
 export default app
